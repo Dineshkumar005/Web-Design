6dc566a237487706c0cb1738e946a4a1865121c6
Kishore S <sskishore67@gmail.com>
2023-03-08 20:28:29 +0530

    sk

--- Main.js
+++ Main.js
@@ -1,1 +1,30 @@
 /* Footer Form */
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+  	var scroll = $(window).scrollTop();
+	  if (scroll > 650) {
+        $(".navbar").css("background" , "#333"); 
+	  }
+      
+
+	  else{
+		   
+          $(".navbar").css("background" , "none");	
+	  }
+  })
+})
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > 650) {
+          $(".logo").css("background" , "white"); 
+        }
+        
+  
+        else{
+             
+            $(".logo").css("background" , "none");	
+        }
+    })
+  })
